cd49368abac95120c5b6f8d17289a3b7f8c692af
Aslan Yeleusiz <101353674+AslanYeleusiz@users.noreply.github.com>
2022-06-21 15:18:58 +0600

    add verified2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -99,24 +99,20 @@ class UserController extends Controller
         $user->email_token = $token;
         $user->save();
         $message = new EmailConfirm($request->email);
-        $headers  = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-        mail($request->email, __('message.Почтаңызды растаңыз'), view('mail.emailUpdate')
+        mail($request->email, __('site.Почтаңызды растаңыз'), view('mail.emailUpdate')
                 ->with([
                     'token' => $token,
                     'email' => $request->email,
-                ]), $headers);
+                ]), null, '-f eduline.kz');
         return;
     }
 
     public function linkToConfirmEmail(Request $request)
     {
-        $headers  = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-        mail($request->email, 'Please confirm email address', view('mail.emailConfirm')
+        mail($request->email, __('site.Почтаңызды растаңыз'), view('mail.emailConfirm')
                 ->with([
                     'email' => $request->email
-                ]), $headers);
+                ]), '-f eduline.kz');
 
         return $request->email;
     }
