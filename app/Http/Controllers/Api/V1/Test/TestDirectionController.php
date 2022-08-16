69e7f154397df4729a2ee0e68b55833e26d1ab51
Aslan Yeleusiz <101353674+AslanYeleusiz@users.noreply.github.com>
2022-08-16 15:24:11 +0600

    apilang

--- app/Http/Controllers/Api/V1/Test/TestDirectionController.php
+++ app/Http/Controllers/Api/V1/Test/TestDirectionController.php
@@ -21,6 +21,15 @@ class TestDirectionController extends Controller
         ->with(['subjects' => fn($query)=>$query->where('language_id', $languageId)])
         ->get();
 
+        if($languageId === 1)
+            foreach($directions as $direction)
+                $direction->name = $direction->name['kk'];
+
+        else if($languageId === 2)
+            foreach($directions as $direction)
+                $direction->name = $direction->name['ru'];
+
+
         return TestDirectionsResource::collection($directions)
         ->additional(['status' => true]);;
     }
