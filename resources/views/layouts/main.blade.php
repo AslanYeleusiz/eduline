f583b97732a272a25ea34ebcb15d0bea73fb2df5
Aslan Yeleusiz <101353674+AslanYeleusiz@users.noreply.github.com>
2022-08-22 17:55:26 +0600

    fixv9

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="{{ asset('css/loader.css?v=1') }}">
     <link rel="stylesheet" href="{{ asset('css/sweetalert2.min.css?v=1') }}">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}?v=1.17">
-    <link rel="stylesheet" href="{{ asset('css/profile.css') }}?v=1.14">
+    <link rel="stylesheet" href="{{ asset('css/profile.css') }}?v=1.16">
     <link rel="stylesheet" href="{{ asset('css/media.css') }}?v=1.1">
     <link rel="stylesheet" href="{{ asset('css/modal.css') }}?v=1.1">
     <link rel="stylesheet" href="{{ asset('css/jquery-ui.min.css') }}">
