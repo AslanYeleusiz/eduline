dc7ab3af6e3330f4d7a3eab974059cf24bf645aa
Aslan Yeleusiz <101353674+AslanYeleusiz@users.noreply.github.com>
2022-08-23 10:28:06 +0600

    try

--- app/Http/Controllers/Api/V1/Test/TestSubjectController.php
+++ app/Http/Controllers/Api/V1/Test/TestSubjectController.php
@@ -27,7 +27,7 @@ class TestSubjectController extends Controller
         $subject = TestSubject::findOrFail($subjectId);
         $questions = TestQuestion::isActive()
         ->subjectBy($subject->id)
-        ->inRandomOrder()->limit($subject->questions_count)->get();
+        ->limit($subject->questions_count)->get();
         return new TestSubjectTestStartResource(compact('subject', 'questions'));
     }
 
