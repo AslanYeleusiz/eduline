85a5acf7fe978c3495fced97b94db1a42a195a7c
Aslan Yeleusiz <101353674+AslanYeleusiz@users.noreply.github.com>
2022-09-09 17:41:09 +0600

    end smtp

--- resources/views/components/SubscriptionModal.blade.php
+++ resources/views/components/SubscriptionModal.blade.php
@@ -26,33 +26,3 @@
         </div>
     </div>
 </div>
-
-
-
-
-<!--
-<div class="modal-header modal-header-auth-padding">
-                <h2 class="modal-title modal-title-second">@lang('site.Төлем жасау әдісін таңдаңыз')</h2>
-                <button type="button" class="close modal-close modal-close-second" data-dismiss="modal" data-bs-dismiss="modal"
-                        aria-label="Close"></button>
-            </div>
-            <div class="modal-body modal-body-second">
-                <div class="pay-variants">
-                    <a href="https://api.whatsapp.com/send?phone=77712345599&text=Сәлеметсізбе! Онлайн платформа бойынша жазып тұрмын"
-                       class="pay-variant-item">
-                        <span class="pay-variant-image kaspi-icon"></span>
-                        @lang('site.Kaspi.kz арқылы')
-                    </a>
-                    <a href="https://api.whatsapp.com/send?phone=77712345599&text=Сәлеметсізбе! Онлайн платформа бойынша жазып тұрмын"
-                       class="pay-variant-item">
-                        <span class="pay-variant-image credit-card-icon"></span>
-                        @lang('site.Картамен төлеу')
-                    </a>
-                    <a href="https://api.whatsapp.com/send?phone=77712345599&text=Сәлеметсізбе! Онлайн платформа бойынша жазып тұрмын"
-                       class="pay-variant-item">
-                        <span class="pay-variant-image whatsapp-icon"></span>
-                        @lang('site.WhatsApp-қа жазу')
-                    </a>
-                </div>
-            </div>
--->
