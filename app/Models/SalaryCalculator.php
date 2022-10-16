8ada28415909b4fa8a19e020585675c5cf47626b
Aslan Yeleusiz <101353674+AslanYeleusiz@users.noreply.github.com>
2022-10-16 15:22:58 +0600

    fix prof

--- app/Models/SalaryCalculator.php
+++ app/Models/SalaryCalculator.php
@@ -12,178 +12,178 @@ class SalaryCalculator extends Model
     const STAVKI = [
         0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2,
     ];
-    const PED_SKILLS = [
+    public PED_SKILLS = [
         [
             'number' => 1,
-            'name' => 'Нет',
+            'name' => __('site.Нет'),
             'percent' => 0
         ],
         [
             'number' => 2,
-            'name' => 'педагог-модератор',
+            'name' => __('site.педагог-модератор'),
             'percent' => 50
         ],
         [
             'number' => 3,
-            'name' => 'педагог-эксперт',
+            'name' => __('site.педагог-эксперт'),
             'percent' => 40
         ],
         [
             'number' => 4,
-            'name' => 'педагог-исследователь',
+            'name' => __('site.педагог-исследователь'),
             'percent' => 35
         ],
         [
             'number' => 5,
-            'name' => 'педагог-мастер',
+            'name' => __('site.педагог-мастер'),
             'percent' => 30
         ]
     ];
 
-    const CATEGORIES = [
+    public CATEGORIES = [
         [
             'number' => 1,
-            'name' => 'Высшая категория'
+            'name' => __('site.Высшая категория')
         ],
         [
             'number' => 2,
-            'name' => '2 '.'категория'
+            'name' => '2 '.__('site.категория')
         ],
         [
             'number' => 3,
-            'name' => '1 '.'категория'
+            'name' => '1 '.__('site.категория')
         ],
         [
             'number' => 4,
-            'name' => 'Без категории'
+            'name' => __('site.Без категории')
         ],
     ];
-    const EDUICATIONS = [
+    public EDUICATIONS = [
         [
-            "name"  => 'Высшее',
+            "name"  => __('site.Высшее'),
             "value" => "B2",
         ],
         [
-            "name"  => 'Среднее специальное',
+            "name"  => __('site.Среднее специальное'),
             "value" => "B4"
         ]
     ];
 
-    const WORK_IN_ENVIRONMENTAL_DISASTER_ZONES = [
+    public WORK_IN_ENVIRONMENTAL_DISASTER_ZONES = [
 
         [
             'number' => 1,
-            'name' => 'Нет',
+            'name' => __('site.Нет'),
             'percent' => 0
         ],
         [
             'number' => 2,
-            'name' => 'Зона экологической катастрофы',
+            'name' => __('site.Зона экологической катастрофы'),
             'percent' => 50
         ],
         [
             'number' => 3,
-            'name' => 'Зона экологического кризиса',
+            'name' => __('site.Зона экологического кризиса'),
             'percent' => 30
         ],
 
         [
             'number' => 4,
-            'name' => 'Зона экологического предкризисного состояния',
+            'name' => __('site.Зона экологического предкризисного состояния'),
             'percent' => 20
         ],
     ];
-    const WORK_IN_RADIATION_RISK_ZONES = [
+    public WORK_IN_RADIATION_RISK_ZONES = [
         [
             'number' => 1,
-            'name' => 'Нет',
+            'name' => __('site.Нет'),
             'mrp' => 0
         ],
         [
             'number' => 2,
-            'name' => 'Зона черезвычайного радиационнного риска',
+            'name' => __('site.Зона черезвычайного радиационнного риска'),
             'mrp' => 2
         ],
         [
             'number' => 3,
-            'name' => 'Зона максимального радиационнного риска',
+            'name' => __('site.Зона максимального радиационнного риска'),
             'mrp' => 1.75
         ],
         [
             'number' => 4,
-            'name' => 'Зона повышенного радиационнного риска',
+            'name' => __('site.Зона повышенного радиационнного риска'),
             'mrp' => 1.5
         ],
 
         [
             'number' => 5,
-            'name' => 'Зона минимального радиационнного риска',
+            'name' => __('site.Зона минимального радиационнного риска'),
             'mrp' => 1.25
         ],
 
         [
             'number' => 6,
-            'name' => 'Зона с льготным социально-экономическим статусом',
+            'name' => __('site.Зона с льготным социально-экономическим статусом'),
             'mrp' => 1
         ],
     ];
 
-    const TECHING_IN_ENGLISH_ITEMS = [
+    public TECHING_IN_ENGLISH_ITEMS = [
 
         [
             'number' => 1,
-            'name' => 'Нет',
+            'name' => __('site.Нет'),
             'bdo' => 0
         ],
         [
             'number' => 2,
-            'name' => 'Частичное погружение' ,
+            'name' => __('site.Частичное погружение') ,
             'bdo' => 1
         ],
         [
             'number' => 3,
-            'name' => 'Полное погружение',
+            'name' => __('site.Полное погружение'),
             'bdo' => 2
         ],
     ];
-    const FOR_MANAGING_OFFICE_ITEMS = [
+    public FOR_MANAGING_OFFICE_ITEMS = [
 
         [
             'number' => 1,
-            'name' => 'Нет',
+            'name' => __('site.Нет'),
             'bdo_percent' => 0
         ],
         [
             'number' => 2,
-            'name' => 'Половина дня',
+            'name' => __('site.Половина дня'),
             'bdo_percent' => 10
         ],
         [
             'number' => 3,
-            'name' => 'Полный день',
+            'name' => __('site.Полный день'),
             'bdo_percent' => 20
         ],
     ];
-    const FOR_CHECKING_NOTEBOOKS =[
+    public FOR_CHECKING_NOTEBOOKS =[
 
         [
             'number' => 1,
-            'name' => 'Нет',
+            'name' => __('site.Нет'),
             'zchbdo_percent' => 0
         ],
         [
             'number' => 2,
-            'name' => 'Учитель начальных классов',
+            'name' => __('site.Учитель начальных классов'),
             'zchbdo_percent' => 50
         ],
         [
             'number' => 3,
-            'name' => 'Учитель ЕМН',
+            'name' => __('site.Учитель ЕМН'),
             'zchbdo_percent' => 50
         ],
         [
             'number' => 4,
-            'name' => 'Учитель языка и литературы',
+            'name' => __('site.Учитель языка и литературы'),
             'zchbdo_percent' => 50
         ],
 
